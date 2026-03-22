984a20714cca5b02eeb5784e6ac1a8bba8784432
jiyun0522 <wnlemforhs24@gmail.com>
2026-03-22 22:14:34 +0900

    [Day05] ⚡ Promise.allSettled 구현하기 - 강지현

--- problems/day05/강지현.js
+++ problems/day05/강지현.js
@@ -1,3 +1,8 @@
+/**
+ * 여러 개의 Promise 또는 값을 병렬로 처리하고, 모든 결과를 배열로 반환
+ * @param {Array<Promise | any>} promises - 실행할 Promise 또는 값의 배열
+ * @returns {Promise<Array>} 각 Promise의 실행 결과 배열
+ */
 async function customPromiseAll(promises) {
   if (promises === null || promises === undefined) {
     return [];
@@ -14,6 +19,44 @@ async function customPromiseAll(promises) {
   return results;
 }
 
+/**
+ * 여러 개의 Promise 또는 값을 병렬로 처리하고, 성공/실패 여부와 관계없이 모든 결과를 반환
+ * @param {Array<Promise | any>} promises - 실행할 Promise 또는 값의 배열
+ * @returns {Promise<Array<{status: string, value?: any, reason?: any}>>} 각 Promise의 성공/실패 여부와 결과를 담은 배열
+ */
+function customPromiseAllSettled(promises) {
+  return new Promise((resolve, reject) => {
+    if (promises === null || promises === undefined) {
+      return resolve([]);
+    }
+
+    const promiseLength = promises.length;
+    const resolvedValue = new Array(promiseLength);
+    let resolveCount = 0;
+
+    if (promiseLength === 0) {
+      return resolve([]);
+    }
+
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then(
+          (value) => {
+            resolvedValue[index] = { status: "fulfilled", value: value };
+          },
+          (reason) => {
+            resolvedValue[index] = { status: "rejected", reason };
+          },
+        )
+        .finally(() => {
+          if (++resolveCount === promiseLength) {
+            resolve(resolvedValue);
+          }
+        });
+    });
+  });
+}
+
 const promise1 = new Promise((resolve) =>
   setTimeout(() => resolve("Result 1"), 1000),
 );
@@ -27,3 +70,9 @@ const promise3 = new Promise((resolve) =>
 customPromiseAll([promise1, promise2, promise3]).then((results) => {
   console.log(results);
 });
+
+customPromiseAllSettled([
+  Promise.resolve("A"),
+  Promise.reject("Error"),
+  123,
+]).then(console.log);
