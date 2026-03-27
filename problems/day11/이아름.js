3e3ae4f435cb1f5cd0702f20ca9c776be878ad5e
aahreum <cocoding420@gmail.com>
2026-03-27 12:15:00 +0900

    [Day11] ⚡ 깊은 복사 함수 구현 - 이아름

--- problems/day11/이아름.js
+++ problems/day11/이아름.js
@@ -13,15 +13,27 @@ const sampleData = {
   ],
 };
 
-function deepClone(obj) {
+function deepClone(obj, cache = new WeakMap()) {
   if (obj === null || typeof obj !== "object") {
     return obj;
   }
 
+  // 순환 참조 문제 해결을 위해 추가
+  // const a = { name: "Alice" };
+  // const b = { name: "Bob" };
+  // a.friend = b;
+  // b.friend = a; < 서로를 참조하는 순환 구조
+  // cache 체크가 없다면? Alice 복사 -> Bob 복사 -> Alice 복사 -> Bob 복사... 무한 복사되는 구조가 되어버림
+  if (cache.has(obj)) {
+    return cache.get(obj);
+  }
+
   const clone = Array.isArray(obj) ? [] : {};
 
+  cache.set(obj, clone);
+
   Object.keys(obj).forEach((key) => {
-    clone[key] = deepClone(obj[key]);
+    clone[key] = deepClone(obj[key], cache);
   });
 
   return clone;
@@ -31,3 +43,12 @@ const test = deepClone(sampleData);
 test.items[0].name = "ahreum";
 console.log(sampleData);
 console.log(test);
+
+const a = { name: "Alice" };
+const b = { name: "Bob" };
+a.friend = b;
+b.friend = a;
+
+const clonedA = deepClone(a);
+
+console.log(clonedA.friend.name); // Bob
