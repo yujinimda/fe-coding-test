ca75929e1531ea2a7e7e34bde31ec79e3f66bfea
jiyun0522 <wnlemforhs24@gmail.com>
2026-03-28 13:29:23 +0900

    [Day12] 제미나이 리뷰 반영

--- problems/day12/강지현.ts
+++ problems/day12/강지현.ts
@@ -15,19 +15,22 @@ function authMiddleware(req: Request, res: Response, next: NextFunction) {
 }
 
 // 예시 요청 객체
-const req = {};
+const req = {} as Request;
 
 // 예시 응답 객체
 const res = {
   redirect: (url: string) => {
     console.log(`Redirecting to ${url}`);
   },
-};
+} as Response;
 
 // 예시 next 함수
 function next() {
   console.log("Access granted to protected route");
 }
 
-// 미들웨어 테스트
+// 미들웨어 테스트 (인증되지 않은 경우)
+authMiddleware(req, res, next);
+// 미들웨어 테스트 (인증된 경우)
+user.isAuthenticated = true;
 authMiddleware(req, res, next);
