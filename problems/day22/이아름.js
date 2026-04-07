2b3a17c271a0739b3c16c738660acfeff24c8c9a
aahreum <cocoding420@gmail.com>
2026-04-07 11:28:16 +0900

    [Day22] 🧩 알고리즘: 다중 조건 정렬을 통한 사용자 목록 정렬 - 이아름

--- problems/day22/이아름.js
+++ problems/day22/이아름.js
@@ -10,19 +10,32 @@ const users = [
 
 // TODO: 여기에 구현하세요
 function sortUsers(users) {
-  const sorted = [...users].sort((a, b) => {
-    if (a.name === b.name && a.age === b.age) {
-      return new Date(b.joined) - new Date(a.joined);
-    }
-
-    if (a.name === b.name) {
-      return a.age - b.age;
-    }
-
-    return a.name.localeCompare(b.name);
-  });
-
-  return sorted;
+  return [...users].sort(
+    (a, b) =>
+      // 1. 이름 오름차순
+      a.name.localeCompare(b.name) ||
+      // 2. 나이 오름차순 (이름이 같을 때)
+      a.age - b.age ||
+      // 3. 가입일 내림차순 (이름과 나이가 같을 때)
+      b.joined.localeCompare(a.joined),
+  );
 }
 
 console.log(sortUsers(users));
+
+/**
+ * 위 동작 방식 이해하기
+ *
+ * 1. 첫 번째 조건 실행
+ * 이름이 다르면 1이나 -1이 반환 > true (이름 오름차순 정렬)
+ * 이름이 만약 같으면 0을 반환 > false여서 다음 조건 확인
+ *
+ * 2. 두 번째 조건 실행
+ * 같은 이름이면서 나이가 다르면 숫자 반환 > true (정렬 확정)
+ * 만약 나이도 같으면 0 > false여서 다음 조건 확인
+ *
+ * 3. 세 번째 조건 실행
+ * 이름, 나이가 모두 같다면 가입일자 기준으로 정렬
+ * date 객체를 만들어서 계산하는 것보다
+ * 2023-01-25 문자열 자체를 비교하는게 성능상 더 이득
+ */
